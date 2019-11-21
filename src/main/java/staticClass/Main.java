e985bc40e4c679042abef381a03cfce4a0b922b4
LukaszBuSDA <bulerluk@gmail.com>
2019-11-21 15:56:46 +0100

    package v3

--- src/main/java/staticClass/Main.java
+++ src/main/java/staticClass/Main.java
@@ -4,18 +4,22 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Main {
     public static void main(String[] args) {
-
-
-        User user1 = new User("x", "x");
-        System.out.println(user1.getLogin());
-
         //Owołanien o skłąowej stataycznej
         // 1. Nie wymaga utworzeni obiektu
         // 2. Odwołujemy się poprzedzając skladowa nazwa klasy
         // 3. Jest tylkok jen okpi askladowej statycznej w rmach klasy
+
+        User user1 = new User("x", "x");
+        System.out.println(user1);
         System.out.println(User.globalId);
+
+
         User user2 = new User("y","y");
-        User.incrementGlobalId();
+        System.out.println(user2);
         System.out.println(User.globalId);
+//        User.incrementGlobalId();
+        System.out.println(User.globalId);
+        User user3 = new User("z","z");
+        System.out.println(user3);
     }
 }
--- src/main/java/staticClass/User.java
+++ src/main/java/staticClass/User.java
@@ -3,26 +3,46 @@ package staticClass;
 
 public class User {
     // sadowe statyczne - pole lub metoda związana z klasą a nie z obiektem
-    public static int globalId;
+    public static int globalId = 1;
+    private int userId;
     private String login;
     private String password;
 
-    public static void incrementGlobalId(){
+    public static void incrementGlobalId() {
         globalId++;
     }
 
     public User(String login, String password) {
+        this.userId = globalId;
         this.login = login;
         this.password = password;
+        incrementGlobalId();
+
 
     }
 
     @Override
     public String toString() {
         return "User{" +
-                "login='" + login + '\'' +
+                "userId=" + userId +
+                ", login='" + login + '\'' +
                 ", password='" + password + '\'' +
                 '}';
     }
+    public String getLogin() {
+        return login;
+
+    }
+    public void setLogin(String login) {
+        this.login = login;
+
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 
 }
